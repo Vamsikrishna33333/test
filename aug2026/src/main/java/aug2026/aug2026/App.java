17e13d148f46350f3fc654e1bcf21bd1eb4a304e
Vamsikrishna33333 <144110603+Vamsikrishna33333@users.noreply.github.com>
2026-08-07 16:34:54 +0530

    Add files via upload

---
+++ aug2026/src/main/java/aug2026/aug2026/App.java
@@ -0,0 +1,13 @@
+package aug2026.aug2026;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
